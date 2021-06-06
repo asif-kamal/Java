cc13079f8238c07743e97b98a6120e488460d185
asif-kamal <asif.kamal1489@gmail.com>
2021-06-06 17:12:46 -0400

    Printed string 5 times. Used while loop.

---
+++ EllieIsClever.java
@@ -0,0 +1,18 @@
+package com.codegym.task.task01.task0102;
+
+/* 
+Kind words for the teacher
+Print string 5 times.
+*/
+
+public class EllieIsClever {
+    public static void main(String[] args) {
+        String s = "Ellie is clever";
+        int n = 0;
+        while (n < 5) {
+        System.out.println(s);
+        n++;
+        //write your code here
+        }
+    }
+}
