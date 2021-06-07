3a98abab9c35bbfadb079063b39f9208b3f342fa
asif-kamal <asif.kamal1489@gmail.com>
2021-06-07 09:40:41 -0400

    Declared 3 variables.

---
+++ DeclareVariables.java
@@ -0,0 +1,15 @@
+package com.codegym.task.task01.task0105;
+
+/* 
+Declare variables
+
+*/
+
+public class DeclareVariables {
+    public static void main(String[] args) {
+        //write your code here
+        String name;
+        int age;
+        String city;
+    }
+}
