077cc12012daa16413101998087656dc9b576064
asif-kamal <asif.kamal1489@gmail.com>
2021-06-09 09:37:26 -0400

    Built PrintDayInWord function, returns weekday

---
+++ PrintDayInWord.java
@@ -0,0 +1,24 @@
+
+public class PrintDayInWord {
+    public static void main (String[] args) {
+        int number = 4; 
+
+        if (number == 1) {
+            System.out.println("Monday");
+        } else if (number == 2) {
+            System.out.println("Tuesday");
+        } else if (number == 3) {
+            System.out.println("Wednesday");
+        } else if (number == 4) {
+            System.out.println("Thursday");
+        } else if (number == 5) {
+            System.out.println("Friday");
+        } else if (number == 6) {
+            System.out.println("Saturday");
+        } else if (number == 7) {
+            System.out.println("Sunday");
+        } else {
+            System.out.println("Not a valid day");
+        }
+    }
+}
