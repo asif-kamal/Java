0021bcf654895b29ab94a667b558908f60573dec
asif-kamal <asif.kamal1489@gmail.com>
2021-06-10 10:23:42 -0400

    Printed out an interpolated string.

---
+++ Greeting.java
@@ -0,0 +1,15 @@
+public class Greeting
+{
+  public static void greet(String name)
+  {
+    // Your code here
+    System.out.println("Hello, " + name + " how are you doing today?");
+  }
+
+
+public static void main(String[] args){
+    greet("Arthur");
+}
+}
+
+//If returning a string no parathesis are needed. In order to print a statemant out you need a public static void method.
